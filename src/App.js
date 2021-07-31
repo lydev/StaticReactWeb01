6f7ccb25e8c4193be3b0a169e1b2d90e2914d4bd
LyDev <lydev@LD.net>
2021-07-30 19:01:40 -0700

    add a small game.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import TicTacToeGame from "./TicTacToe"
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      <h1>Static Web App React01 on GSW</h1>
+      <div>Hello {value}</div>
+      <hr></hr>
+      <TicTacToeGame></TicTacToeGame>
+    </div>
+  );
 }
 
 export default App;
